ea18b1472c0d6e0fa72bad1d35020b8dc7ab24e1
Ramesh Kumar <rameshkumar455555@gmailcom>
2023-01-29 03:34:04 +0530

    Updated

--- Array/Smllest.cpp
+++ Array/Smllest.cpp
@@ -1,5 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+/*
+Problem Statement: Given an array, we have to find the smallest element in the array.
+
+Examples:
+
+Example 1:
+Input: arr[] = {2,5,1,3,0};
+Output: 0
+Explanation: 0 is the smallest element in the array. 
+
+Example2: 
+Input: arr[] = {8,10,5,7,9};
+Output: 5
+Explanation: 5 is the smallest element in the array.
+*/
 int smallest2(vector<int>arr)
 {
     int min=INT_MAX;
--- Array/max.cpp
+++ Array/max.cpp
@@ -1,5 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+/*
+Problem Statement: Given an array, we have to find the largest element in the array.
+
+Examples:
+
+Example 1:
+Input: arr[] = {2,5,1,3,0};
+Output: 5
+Explanation: 5 is the largest element in the array. 
+
+Example2: 
+Input: arr[] = {8,10,5,7,9};
+Output: 10
+Explanation: 10 is the largest element in the array. 
+*/
 int maxarr2(vector<int>arr)
 {
     int max=arr[0];
